9ccfb36d2ebe0f184e516caefc45b0414e434a14
krishnaghosh2081 <krishna.ghosh2081@gmail.com>
2026-06-15 10:58:32 +0200

    add logo

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,11 @@
 import AddEntry from "./AddEntry";
+import marc from "../assets/logo.avif";
 
 export default function Navbar() {
   
   return (
-    <div className="bg-green-300 p-10 rounded-8px shadow   mx-auto text-blue-600 flex h-45px items-center justify-between">
-      <span>Logo</span>
+    <div className="bg-green-300 p-10 rounded-8px shadow   mx-auto text-blue-600 flex  h-[150px] items-center justify-between">
+      <span className="w-[100px] h-[100px]"><img src={marc} alt="logo" /></span>
       <h2 className="text-2xl font-bold text-center mb-6">
         Daily Routine
       </h2>    
